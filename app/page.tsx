013d310324b4a3b551758c7754cc9d8d31e4c209
fschlegelone <fabian@schlegel.one>
2024-03-04 13:28:29 +0100

    feat: Add Skills Section

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import HeroSection from "@/components/views/hero-section";
 import AboutMe from "@/components/views/about-me";
 import Projects from "@/components/views/projects";
-
+import Skills from "@/components/views/skills";
 export default function Page() {
   return (
     <div id="Home" className="pb-10">
       <HeroSection />
       <AboutMe />
       <Projects />
+      <Skills />
     </div>
   );
 }
---
+++ components/views/skills.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useSectionInView } from "@/lib/hooks";
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { developmentSkills, softSkills, technologicalSkills } from "@/data/data";
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+export default function Skills() {
+  const { ref } = useSectionInView("Skills");
+  const fadeInAnimationVariants = {
+    initial: {
+      opacity: 0,
+      x: -50,
+    },
+    animate: (index: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: 0.05 * index,
+      },
+    }),
+  };
+
+  return (
+    <div ref={ref} id="skills" className="mx-auto max-w-6xl pt-28 p-4">
+      <p className="text-4xl font-semibold">Skills</p>
+
+      <p className="mb-2 mt-5 font-bold text-black/60 dark:text-white/60">Development Skills</p>
+      <div className="flex flex-wrap gap-2">
+        {developmentSkills.map((skill, index) => (
+          <motion.div
+            key={index}
+            className=" rounded-xl bg-secondary/80 p-5 py-2 md:text-lg font-semibold text-black dark:text-white"
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.div>
+        ))}
+      </div>
+
+      <p className="mb-2 mt-10 font-bold text-white/60">Technological Skills</p>
+      <ul className="flex flex-wrap gap-2">
+        {technologicalSkills.map((skill, index) => (
+          <motion.li
+            className=" rounded-xl bg-secondary/80 p-5 py-2 md:text-lg font-semibold text-black dark:text-white"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+
+      <p className="mb-2 mt-10 font-bold text-white/60">Soft Skills</p>
+      <ul className="flex flex-wrap gap-2">
+        {softSkills.map((skill, index) => (
+          <motion.li
+            className=" rounded-xl bg-secondary/80 p-5 py-2 md:text-lg font-semibold text-black dark:text-white"
+            key={index}
+            variants={fadeInAnimationVariants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
+    </div>
+  );
+}
